ec5147b6e044fe7fd14b6a3ab3dc830967ec408a
Sri Harsha Chakravarthy Tenali <125214865+tenalisriharsha@users.noreply.github.com>
2025-12-07 15:15:18 -0600

    Remove unused import of SwipeableViews

--- src/Pages/Portofolio1.jsx
+++ src/Pages/Portofolio1.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState, useCallback } from "react";
 import { supabase } from "../supabase"; 
 
 import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
 import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Tabs from "@mui/material/Tabs";
